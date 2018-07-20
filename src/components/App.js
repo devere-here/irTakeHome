80c1ef9a35cc5d4e61eeecdf2ac176aa2794f8cd
devere-here <stevendeverehere@gmail.com>
2018-07-20 13:29:27 -0400

    feat: receives api data

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Search from './Search'
 import DefaultDisplay from './DefaultDisplay'
 import Albums from './Albums'
+import axios from 'axios'
 
 class App extends React.Component{
     state = {
@@ -9,11 +10,19 @@ class App extends React.Component{
         searched: false
     }
 
+    handleSearch = (searchTerm) => {
+        axios.get(`https://itunes.apple.com/search?term=${searchTerm}`)
+        .then(res => {
+            console.log('res is', res.data.results)
+        })
+        .catch(err => console.log(err))
+    }
+
     render = () => {
         let { albums, searched } = this.state
         return (
             <div>
-                <Search />
+                <Search handleSearch={this.handleSearch} />
                 {searched
                     ? <Albums />
                     : <DefaultDisplay />
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-const Search = () => (
-    <h1>In Search</h1>
-)
+
+class Search extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            searchTerm: ''
+        }
+    }
+
+    handleChange = evt => {
+        console.log('in handle change', evt.target.value)
+        this.setState({searchTerm: evt.target.value})
+    }
+
+    render = () => {
+        let { handleSearch } = this.props 
+        
+        return (
+            <div>
+                <h1>In Search</h1>
+                <input value={this.state.searchTerm} onChange={this.handleChange}/>
+                <button onClick={handleSearch}>Submit</button>
+            </div>
+        )   
+    }
+}
 
 export default Search
